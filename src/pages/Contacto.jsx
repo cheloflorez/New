c2a369a4e48b4108e861d7a70ff4b57fa143cc64
Marcelo <marcelo.florez@gmail.com>
2022-10-30 15:35:48 -0300

    Update

--- src/pages/Contacto.jsx
+++ src/pages/Contacto.jsx
@@ -35,12 +35,6 @@ const Contacto = ({ greeting }) => {
                                 </div>
                             </form>
                         </div>
-                        <div className="col-md-6">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3398.7473422443904!2d-60.715613984308874!3d-31.585975711637033!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95b5070f71e30c7f%3A0x510f44df7ebec165!2sAv.%20Jos%C3%A9%20Gorriti%204387%2C%20S3006%20DMC%2C%20Santa%20Fe!5e0!3m2!1ses-419!2sar!4v1654648061222!5m2!1ses-419!2sar"  width={600} height={450} style={{ border: 0 }} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
-                            <p>Argentina</p>
-                            <p>+1 11110000000</p>
-                            <p>contacto@burger.com</p>
-                        </div>
                     </div>
                 </div>
             </main>
